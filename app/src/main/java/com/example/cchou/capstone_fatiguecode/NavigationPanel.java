3b89535b88fd91e331d604cbf3e508d905a5260c
Catherine <cchou89@gatech.edu>
2017-11-18 13:59:23 -0500

    hi comment added

--- app/src/main/java/com/example/cchou/capstone_fatiguecode/NavigationPanel.java
+++ app/src/main/java/com/example/cchou/capstone_fatiguecode/NavigationPanel.java
@@ -15,7 +15,7 @@ import android.view.MenuItem;
 
 public class NavigationPanel extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+//hi im a comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
